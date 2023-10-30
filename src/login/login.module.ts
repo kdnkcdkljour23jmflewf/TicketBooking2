d9ba2cfab352fb7eaa7e7055b19bae94074ed393
kdnkcdkljour23jmflewf <guptadharmanshu@gmail.com>
2023-10-30 23:23:08 +0530

    Login register module created functionality remaining

--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { LoginController } from './login.controller';
+import { LoginService } from './login.service';
 
-@Module({})
+@Module({
+  controllers: [LoginController],
+  providers: [LoginService]
+})
 export class LoginModule {}
---
+++ src/register/register.controller.ts
@@ -0,0 +1,6 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('register')
+export class RegisterController {
+    
+}
