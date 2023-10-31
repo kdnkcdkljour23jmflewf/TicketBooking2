5e7d5c65912f92feb63d922d850970d188b7517c
kdnkcdkljour23jmflewf <guptadharmanshu@gmail.com>
2023-10-31 23:39:52 +0530

    Register table done

--- src/register/register.module.ts
+++ src/register/register.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RegisterController } from './register.controller';
 import { RegisterService } from './register.service';
+import { Register } from './register.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Register])],
   controllers: [RegisterController],
   providers: [RegisterService]
 })
--- src/register/register.service.ts
+++ src/register/register.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm'; // Ensure this import
+import { Repository } from 'typeorm';
+import { Register } from './register.entity';
 
 @Injectable()
-export class RegisterService {}
+export class RegisterService {
+    constructor(
+        @InjectRepository(Register) // Inject the repository for the Movie entity
+        private registerRepository: Repository<Register>,
+      ) {}
+}
