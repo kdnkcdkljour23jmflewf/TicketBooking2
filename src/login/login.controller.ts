d1d0258df3972f3198b938ac7a332c98f704479f
kdnkcdkljour23jmflewf <guptadharmanshu@gmail.com>
2023-12-20 20:59:01 +0530

    Login done with token

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,23 +1,32 @@
 import { Controller,Post,Body, UseGuards,Request } from '@nestjs/common';
 // import { LocalAuthGuard } from './local-auth.guard';
 import { LoginService } from './login.service';
-// import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt'
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('login')
 export class LoginController {
-    constructor(private LoginService:LoginService){
+    constructor(private LoginService:LoginService,
+        private readonly jwtService: JwtService,
+        ){
         
     }
 //  @UseGuards(LocalAuthGuard)
  @Post('login')
     async  login(@Request() req) {
         let {email,password} = req.body
-            const payload = { email: email, password: password }
+            
             const user = await this.LoginService.findOneByUsername(email)
-            console.log(9999,user)
-        //     return {
-        //       access_token: this.jwtService.sign(payload),
-        //     }
-        //  }
+            if(!user){
+                return { message: 'Invalid credentials' }
+            }
+            const password_match = await bcrypt.compare(password,user.password)
+            if(!password_match){
+                return { message: 'Password does not match' }
+            }            
+            const payload = { email: email, password: password }
+            return {
+                access_token: this.jwtService.sign(payload),
+            }
     }
 }
--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -3,9 +3,17 @@ import { LoginController } from './login.controller';
 import { LoginService } from './login.service';
 import { Register } from '../register/register.entity'
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Register])],
+  imports: 
+  [
+    TypeOrmModule.forFeature([Register]),
+    JwtModule.register({
+      secret: 'your-secret-key', // replace with your actual secret key
+      signOptions: { expiresIn: '2h' }, // you can adjust the expiration time
+    })
+  ],
   controllers: [LoginController],
   providers: [LoginService]
 })
